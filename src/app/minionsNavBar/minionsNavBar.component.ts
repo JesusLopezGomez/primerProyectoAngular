2cf82d7ef779e2a6ad1022f81edd7a5895f7165a
JesuLopezGomez <jelopez.gom@gmail.com>
2023-12-18 12:26:09 +0100

    .

--- src/app/minionsNavBar/minionsNavBar.component.ts
+++ src/app/minionsNavBar/minionsNavBar.component.ts
@@ -1,14 +1,22 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-minionsNavBar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [FormsModule],
   templateUrl: './minionsNavBar.component.html',
   styleUrl: './minionsNavBar.component.css'
 })
 
 export class MinionsNavBarComponent {
   termino:string = ""; 
+
+  @Output() emitir:EventEmitter<string> = new EventEmitter<string>();
+
+  setTermino(){
+    
+    this.emitir.emit(this.termino);
+  }
 }
